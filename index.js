cd54a387feb564b3f255252ab2f941e980936ce6
camilalindademais1-cyber <camilalindademais1@gmail.com>
2026-01-30 21:11:21 -0300

    Refactor server port configuration

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.post("/criar-pix", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
 });
